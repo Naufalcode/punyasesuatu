bfe4e2529e0fa8d8d95df7ea6f814b73abd38897
Naufalcode <naufalcogan913@gmail.com>
2024-09-17 17:59:50 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Gita Cantik'.split('')
+const text = 'I Have Something !!!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
